de6b63cfa9cacffda79230aa954bd6cee89f6976
abdielreyhan <abdielreyhan98@gmail.com>
2020-08-21 14:48:28 +0700

    auth

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -1,26 +1,20 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    
-
-    <div class="content">
-        <div class="title m-b-md">
-            Pizza List
-        </div>
+    <div class="wrapper pizza-index">
+        <h1>Pizza Order</h1>
         <!-- @for($i=0; $i<count($type_pizza); $i++)
             <p> {{$type_pizza[$i]['type']}} </P>
         @endfor -->
-    <!-- loop can be index(to shoq index of looping) or last(to show last looping type boolean) -->
+        <!-- loop can be index(to shoq index of looping) or last(to show last looping type boolean) -->
 
         @foreach($type_pizza as $pizza)
-            <div>
-                {{$pizza->name}} - {{$pizza->type}} - {{$pizza->base}}
+            <div class="pizza-item">
+                <img src="/img/pizzalogo.png" alt="Pizza Order">
+                <h4><a href="/pizzas/{{$pizza->id}}">{{$pizza->name}} - {{$pizza->type}} - {{$pizza->base}}</a></h4>
             </div>
         @endforeach
 
         <p class="mssg">{{session('mssg')}}</p>
-
     </div>
-</div>
 @endsection
